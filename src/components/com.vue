e60dd14611a00fa26771dcb02f8e3c0d7383c5fd
Jack Yang <ygjack414@hotmail.com>
2016-07-25 22:54:55 +0800

    add

--- src/components/com.vue
+++ src/components/com.vue
@@ -29,6 +29,37 @@
         Vue.krCall('set', hotspotVarName + '.point[1].atv', 5);
         Vue.krCall('set', hotspotVarName + '.point[2].ath', 10);
         Vue.krCall('set', hotspotVarName + '.point[2].atv', 10);
+
+        var polygonHotspot = new PolygonHotspot({
+            name: '',
+            fillAlpha: '',
+            points: [],
+            onClick: function () {
+                krCall('removehospot', this.name);
+            },
+            onDown: function () {
+                dragHotspot(this);
+            }
+        });
+
+        shell.addHospot(polygonHotspot);
+    }
+
+    function dragHotspot (hotspot) {
+        /*
+         <action name="draghotspot">
+         spheretoscreen(ath, atv, hotspotcenterx, hotspotcentery, 'l');
+         sub(drag_adjustx, mouse.stagex, hotspotcenterx);
+         sub(drag_adjusty, mouse.stagey, hotspotcentery);
+         asyncloop(pressed,
+            sub(dx, mouse.stagex, drag_adjustx);
+            sub(dy, mouse.stagey, drag_adjusty);
+            screentosphere(dx, dy, ath, atv);
+            print_hotspot_pos();
+         );
+         </action>
+        */
+        Vue.krCallWith(hotspot.varPath, )
     }
 
     module.exports = {
